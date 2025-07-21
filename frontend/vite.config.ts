d7e74cdcacaedc13634fcf1a8716b86780bcd228
aravindsvpc <88551129+aravindsvpc@users.noreply.github.com>
2025-07-21 22:48:53 +0530

    Update port on vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173, // Render will override this with process.env.PORT
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
