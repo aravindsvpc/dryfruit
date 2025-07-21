2a845ce99f97dcc60a44cc25723f61a352e779af
aravindsvpc <88551129+aravindsvpc@users.noreply.github.com>
2025-07-21 22:52:52 +0530

    Update allowedhosts vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173, // Render will override this with process.env.PORT
+    allowedHosts: ['srisakhambari.onrender.com'],
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
